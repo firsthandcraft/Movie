b87a74c723128a6ab6b4e1d1f6785e434b5ecc81
handcraft <opaldust19@gmail.com>
2024-04-12 14:47:03 +0900

    pagenation넣기

--- movie/src/main.js
+++ movie/src/main.js
@@ -1,6 +1,7 @@
 import './assets/reset.css'
 import './assets/main.css'
-
+import VueAwesomePaginate from "vue-awesome-paginate";
+import "vue-awesome-paginate/dist/style.css";
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -11,5 +12,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueAwesomePaginate);
 
 app.mount('#app')
--- movie/src/views/HomeView.vue
+++ movie/src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 import {onBeforeMount} from 'vue'
 import {useMovieStore} from '@/stores/movie.js'
 import '../assets/main.scss';
 
 const movieStore = useMovieStore();
 let sortValue="popularity.desc";
+
 onBeforeMount(()=>{
 movieStore.getMoviesList();
-})
+  movieStore.getMoviesList();
+
+});
+
 const getMovie=(num)=>{
   movieStore.getMoviesList(num,sortValue);
 }
@@ -16,6 +21,10 @@ const handleChange = (event) => {
   getMovie(1);
 }
 
+const onClickHandler = (page) => {
+    console.log(page);
+  };
+const currentPage = ref(1);
 </script>
 
 <template>
@@ -47,8 +56,49 @@ const handleChange = (event) => {
             </router-link>
           </li>
         </ul>
+        <div aria-label="Page navigation example">
+            eddd
+            <vue-awesome-paginate
+                :total-items="500"
+                v-model="currentPage"
+                :items-per-page="5"
+                :max-pages-shown="5"
+                :show-breakpoint-buttons="false"
+                :show-jump-buttons="true"
+                @click="getMovie(currentPage)"
+              />
+
+   
+        </div>
       </div>
     </div>
    
   </main>
 </template>
+
+<style>
+  .pagination-container {
+    display: flex;
+    column-gap: 10px;
+  }
+  .paginate-buttons {
+    height: 40px;
+    width: 40px;
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: rgb(242, 242, 242);
+    border: 1px solid rgb(217, 217, 217);
+    color: black;
+  }
+  .paginate-buttons:hover {
+    background-color: #d8d8d8;
+  }
+  .active-page {
+    background-color: #3498db;
+    border: 1px solid #3498db;
+    color: white;
+  }
+  .active-page:hover {
+    background-color: #2988c8;
+  }
+</style>
